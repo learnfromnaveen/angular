9d98cd6507f30e1526a2955535f60353fa686777
Naveen-NN <nn.naveen@gmail.com>
2019-04-10 07:57:55 +0530

    HttpClient

--- my-app/src/app/hero.service.ts
+++ my-app/src/app/hero.service.ts
@@ -1,22 +1,57 @@
 import { Injectable } from '@angular/core';
-import { HEROES } from './mock-heroes'
 import { Hero } from './hero'
 import { Observable, of } from 'rxjs'
 import { MessageService } from './message.service'
+
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { catchError, map, tap } from 'rxjs/operators'
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  constructor(
+      private messageService: MessageService,
+      private http: HttpClient
+    
+    ) { }
 
   getHeroes(): Observable<Hero[]>{
-    this.messageService.add('HeroService: fetched');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+            .pipe(
+                tap(data => {
+                    this.log('HeroService: fetched');
+                }),
+                catchError(this.handleErrror<Hero[]>('getHeroes',[]))
+            );
   }
 
   getHero(id: number): Observable<Hero>{
-    this.messageService.add(`HeroService: fetched id:${id}`);
-    return of(HEROES.find( hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url)
+            .pipe(
+                 tap( data =>{
+                    this.log(`HeroService: fetched id:${id}`);
+                 }),
+                 catchError(this.handleErrror<Hero>(`getHero id: ${id}`))
+            );
+  }
+
+  private log(message: string): void{ 
+    this.messageService.add(message);
+
+  }
+
+
+  private handleErrror<T>(operation = 'operation', result?: T){
+    return (error: any): Observable<T> =>{
+      var message: string = `${operation} failed: ${error.statusText}`;
+      console.log( message );
+      this.log(message);
+      return of(result as T);
+    }
   }
 }
